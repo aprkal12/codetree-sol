fbd183482e6966fbc4f9f83b1f2bc757b4ded691
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-08-14 14:08:37 +0900

    [Passed] 최대로 겹치는 지점 (68ms, 16MB)

---
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+    segments.push(input[i].split(' ').map(Number));
+}
+
+// Please Write your code here.
+const arr = new Array(100).fill(0)
+let result = 0;
+for (const seg of segments) {
+    for (let i = seg[0]; i <= seg[1]; i++) {
+        arr[i] += 1;
+    }
+}
+for (let i = 0; i < arr.length; i++) {
+    if (result < arr[i]) {
+        result = arr[i];
+    }
+}
+console.log(result);
