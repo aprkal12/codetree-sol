8415645be11361c5c7f25d23a6fdc5b3e875acb2
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-08-23 23:00:42 +0900

    [Passed] 최고의 13위치 2 (88ms, 15MB)

---
+++ trail2/최고의 13위치 2/best-place-of-13-2.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please write your code here.
+let result = Number.MIN_SAFE_INTEGER;
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 2; j++) {
+        let count = 0;
+        for (let k = 0; k < 3; k++) {
+            count += grid[i][j + k] > 0 ? 1 : 0;
+        }
+        let midcount = 0;
+        for (let k = 0; k < n; k++) {
+            if (i === k && j + 6 <= n) {
+                for (let l = j + 3; l < j + 6; l++) {
+                    midcount += grid[k][l] > 0 ? 1 : 0;
+                }
+                result = Math.max(result, count + midcount)
+                midcount = 0;
+            }
+            if (i < k) {
+                for (let l = 0; l < n - 2; l++) {
+                    for (let m = 0; m < 3; m++) {
+                        midcount += grid[k][l + m] > 0 ? 1 : 0;
+                    }
+                    result = Math.max(result, count + midcount)
+                    midcount = 0;
+                }
+            }
+        }
+    }
+}
+console.log(result);
