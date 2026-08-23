06444c46e28bbdef653250b7df4eef8864791332
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-08-23 13:34:18 +0900

    [Passed] 씨 오 더블유 2 (67ms, 10MB)

---
+++ trail2/씨 오 더블유 2/c-o-w-2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const string = input[1];
+
+// Please Write your code here.
+let result = 0;
+for (let i = 0; i < n - 2; i++) {
+    if (string[i] === 'C') {
+        for (let j = i + 1; j < n - 1; j++) {
+            if (string[j] === 'O') {
+                for (let k = j + 1; k < n; k++) {
+                    if (string[k] === 'W') {
+                        result++;
+                    }
+                }
+            }
+        }
+    }
+}
+console.log(result)
