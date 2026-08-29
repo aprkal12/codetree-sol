fa53107b77906e64e7c3ebaad305ce3692b33e83
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-08-30 02:12:48 +0900

    [Passed] 이상한 폭탄 2 (64ms, 10MB)

---
+++ trail2/이상한 폭탄 2/strange-bomb-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input.slice(1, n + 1).map(Number);
+
+// Please Write your code here.
+let max = -1;
+for(let i = 0; i<n; i++){
+    for(let j = 0; j<n; j++){
+        if(i !== j && nums[i] === nums[j] && Math.abs(i - j) <= k){
+            max = Math.max(max, nums[i]);
+        }
+    }
+}
+console.log(max)
