6c73205e7bdfe40ef5ce6d521af588140993d81a
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-09-08 21:52:00 +0900

    [Passed] 빙산의 일각 2 (67ms, 10MB)

---
+++ trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const h = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+let maxh = Math.max(0, ...h);
+let minh = Math.min(0, ...h);
+let result = 0;
+for (let i = minh; i < maxh; i++) {
+    let count = 0;
+    let block = false;
+    for (let j = 0; j < n; j++) {
+        if (h[j] > i && !block) {
+            count++;
+            block = true;
+        } else if (h[j] <= i) {
+            block = false;
+        }
+    }
+    result = Math.max(result, count);
+}
+console.log(result)
