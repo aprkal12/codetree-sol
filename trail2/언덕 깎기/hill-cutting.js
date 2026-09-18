607f483bd905ce4c1d35f409574db3ca678e4045
DongHyun Kim <49503858+aprkal12@users.noreply.github.com>
2026-09-18 18:01:58 +0900

    [Passed] 언덕 깎기 (65ms, 10MB)

---
+++ trail2/언덕 깎기/hill-cutting.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const hills = [];
+
+for (let i = 1; i <= n; i++) {
+    hills.push(Number(input[i]));
+}
+
+// Please Write your code here.
+let min = Math.min(...hills);
+let max = Math.max(...hills);
+let result = Number.MAX_SAFE_INTEGER;
+for (let i = min; i < max; i++) {
+    let cost = 0;
+    for (let j = 0; j < n; j++) {
+        let num = hills[j];
+        if (num < i) {
+            cost += (i - num) * (i - num);
+        } else if (num > (i + 17)) {
+            cost += (num - (i + 17)) * (num - (i + 17));
+        }
+    }
+    result = Math.min(result, cost);
+}
+console.log(result);
+
